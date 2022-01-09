1f6e286278a9c1b208c45d08bd0125459f01b255
Diaphara DRAME <drammediaphara@gmail.com>
2022-01-09 10:02:49 +0100

    immobilier

--- haut_de_page.php
+++ haut_de_page.php
@@ -16,15 +16,13 @@
 
 <body>
     <!----------------------------------------------le debut du header----------------------------------------------------->
-    <header>
-        <nav class="navbar navbar-expand-lg">
+    <header class="navbar navbar-expand-lg">
             <div class="container-fluid">
-                <a class="navbar-brand" href="index.php">Accueil</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
+                        <li class="nav-item">
+                        <a class="navbar-brand" href="index.php">Accueil</a>
+                        </li>
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="formulaire_inscription.php">Inscription</a>
                         </li>
@@ -40,10 +38,10 @@
                     </ul>
                 </div>
             </div>
-        </nav>
+
     </header>
 
-    <nav class="navbar navbar-expand-lg ">
+    <nav class="monnav navbar-expand-lg ">
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <a class="navbar-brand" href="#">Menu</a>
